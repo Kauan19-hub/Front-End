42ed47c6cc7ccece2cc107f17cd5cd44c27158a8
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2025-10-28 16:06:42 -0300

    React Class

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,11 +1,1 @@
-import style from '../styles/cabecalho.module.css';
 
-// Export function (name of your file)
-export function cabecalho() {
-    return (
-        <header className={style.header}>
-            <h1 className={style.title}>VIDEO AQUI</h1>
-        </header>
-
-    )
-}
