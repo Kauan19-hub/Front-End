8b5995bb9ed333e252433907f5b0c7541be3da6e
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2025-10-31 14:11:00 -0300

    Book

--- src/pages/livro/livro.jsx
+++ src/pages/livro/livro.jsx
@@ -17,16 +17,37 @@ export function Livro() {
 
                 <section className={style.content}>
                     <div className={style.text}>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit magni sunt nostrum animi cupiditate tempora, sed, numquam distinctio maiores nesciunt eveniet illum quasi voluptas rerum doloremque neque itaque quod exercitationem.
+                         <p>
+                            Crepúsculo poderia ser uma história comum, não fosse um elemento irresistível: o objeto da paixão da
+                            protagonista é um vampiro. Assim, soma-se à paixão um perigo sobrenatural temperado com muito suspense,
+                            e o resultado é uma leitura de tirar o fôlego. Um romance repleto das angústias e incertezas da
+                            juventude - o arrebatamento, a atração, a ansiedade que antecede cada palavra, cada gesto, e todos os medos.
                         </p>
 
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum minus fuga quo omnis, aperiam itaque a vitae laboriosam consectetur, voluptatibus distinctio nostrum. Alias eum neque minima adipisci eligendi fugiat blanditiis.
+                            Isabella Swan chega à nublada e chuvosa cidadezinha de Forks - último lugar onde gostaria de viver.
+                            Tenta se adaptar à vida provinciana na qual aparentemente todos se conhecem, lidar com sua constrangedora
+                            falta de coordenação motora e se habituar a morar com um pai com quem nunca conviveu. 
+                            Em seu destino está Edward Cullen.
                         </p>
 
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ipsum repellendus sed. Debitis excepturi facere doloremque tenetur sequi aliquid, rerum at quod eveniet nemo accusamus! Facere animi similique quis eos.
+                            Lindo, perfeito, misterioso, ele é à primeira vista, hostil à presença de Bella - o que provoca nela uma
+                            inquietação desconcertante. Ela se apaixona. Ele, no melhor estilo "amor proibido", alerta: Sou um risco para você.
+                            Ela é uma garota incomum. Ele é um vampiro. Ela precisa aprender a controlar seu corpo quando ele a toca. Ele,
+                            a controlar sua sede pelo sangue dela. Em meio a descobertas e sobressaltos, Edward é, sim, perigoso: um perigo
+                            que qualquer mulher escolheria correr.
+                        </p>
+
+                        <p>
+                            Nesse universo fantasioso, os personagens construídos por Stephenie Meyer - humanos ou não - se mostram de tal forma
+                            familiares em seus dilemas e em seu comportamento que o sobrenatural parece real. Meyer torna perfeitamente plausível
+                            - e irresistível - a paixão de uma garota de 17 anos por um vampiro encantador.
+                        </p>
+
+                        <p>
+                            Com direitos vendidos para mais de 40 países e para o cinema, Crepúsculo chegou às telas em 2008, em filme assinado
+                            pelos produtores de O Diabo Veste Prada, protagonizado por Robert Pattinson e Kristen Stewart e dirigido por Catherine Hardwicke.
                         </p>
                     </div>
                 </section>
